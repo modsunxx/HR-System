516b75bf1cb1536d21fdbf2f414d48018c7d8ca2
modsunxx <modsunxx@gmail.com>
2026-09-04 23:06:17 +0700

    fix :userprofile avatar doesn,t show

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -3,22 +3,24 @@ interface UserProfileProps {
   name: string;
   role: string;
   username: string;
+  avatarUrl?: string | null;
 }
 
 export default function UserProfile({
   name,
   role,
   username,
+  avatarUrl,
 }: UserProfileProps) {
-  // เอา username มาทำเป็นรูป Avatar แบบสุ่มให้ไม่ซ้ำกัน
-  const avatarUrl = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}&backgroundColor=transparent`;
-
+  const displayAvatar = avatarUrl
+    ? avatarUrl
+    : `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}&backgroundColor=transparent`;
   return (
     <div className="flex flex-col items-center mb-8 pb-8 border-b border-white/20">
       <div className="w-20 h-20 rounded-full mb-4 overflow-hidden border-2 border-white/30 shadow-lg bg-white/5">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src={avatarUrl}
+          src={displayAvatar} // 🌟 4. เปลี่ยนมาใช้ตัวแปร displayAvatar ที่เราเช็คไว้
           alt="Profile"
           className="w-full h-full object-cover"
         />
